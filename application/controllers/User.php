9720bd30f8958615726241f98cf168bbda8f9c15
praveen tissera <praveen.tissera@gmail.com>
2019-04-03 21:17:09 +0530

    add assets folder and create controler and model

---
+++ application/controllers/User.php
@@ -0,0 +1,25 @@
+<?php
+Class User extends CI_Controller {
+    public function __construct() {
+		parent::__construct();
+		// Load form helper library
+		$this->load->helper('form');
+		// Load form validation library
+		$this->load->library('form_validation');
+		// Load session library
+		$this->load->library('session');
+		//load url library
+		$this->load->helper('url');
+		// Load database
+		$this->load->model('User_model');
+		
+		date_default_timezone_set("Asia/colombo");
+	//	$this->load->model('user_model');
+	
+		$this->load->library('upload');
+		
+    } 
+    public function index(){
+		$this->load->view('home');
+    }
+}
